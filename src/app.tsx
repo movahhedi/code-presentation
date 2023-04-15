675e4e2d11564c9b55ce9041bdaf53baeb494162
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2023-04-15 12:05:32 +0330

    Made Line Numbers Dynamic

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,72 @@
-import $ from "jquery";
+function trigger(el: Element, eventType: string) {
+	if (typeof eventType === "string" && typeof el[eventType] === "function") {
+		el[eventType]();
+	} else {
+		const event = typeof eventType === "string" ? new Event(eventType, { bubbles: true }) : eventType;
+		el.dispatchEvent(event);
+	}
+}
+
+function docReady(fn) {
+	// see if DOM is already available
+	if (document.readyState === "complete" || document.readyState === "interactive") {
+		// call on next available tick
+		setTimeout(fn, 1);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
 const ProjectDirectory = import.meta.env.VITE_CONTENT_NAME;
 
 const Delimiter = "----------------------------------------------------\r\n",
 	KeyCode_Left = 37,
 	KeyCode_Up = 38,
 	KeyCode_Right = 39,
-	KeyCode_Down = 40;
+	KeyCode_Down = 40,
+	MinLineNumbers = 2;
 
 let Name = "",
 	Page = 0,
 	AllPages = 100,
 	IsLightMode = false,
 	Pages = [];
+
+const LineNumbersBox = <div id="LineNumbers"></div>;
+const TypeTextarea = (
+	<textarea
+		id="Input"
+		spellCheck={false}
+		placeholder=" "
+		rows={1}
+		onInput={(e) => {
+			e.currentTarget.style.height = "25px";
+			e.currentTarget.style.height = e.currentTarget.scrollHeight + "px";
+			e.currentTarget.removeAttribute("rows");
+			SetLineNumbers();
+		}}
+		onKeyDown={(e) => {
+			e.currentTarget.spellcheck = false;
+			if (e.key == "Tab") {
+				// get caret position/selection
+				const target = e.currentTarget as HTMLTextAreaElement;
+				const start = target.selectionStart ?? 0;
+				const end = target.selectionEnd ?? 0;
+
+				const value = "" + e.currentTarget.value;
+
+				// set textarea value to: text before caret + tab + text after caret
+				e.currentTarget.value = value.substring(0, start) + "\t" + value.substring(end);
+
+				// put caret at right position again (add one for the tab)
+				target.selectionStart = target.selectionEnd = start + 1;
+
+				// prevent the focus lose
+				e.preventDefault();
+			}
+		}}
+	></textarea>
+) as HTMLTextAreaElement;
 
 const LoadContent = async () => {
 	Name = (await import(`./content/${ProjectDirectory}/name.txt?raw`)).default;
@@ -20,9 +75,21 @@ const LoadContent = async () => {
 	AllPages = Pages.length - 1;
 };
 
-const LoadPage = (page = 1) => {
-	$("#MainContent").html(Pages[page]);
-	setTimeout(() => $("textarea").trigger("input"), 100);
+const LoadPage = (page = 0) => {
+	document.getElementById("MainContent").innerHTML = Pages[page] as string;
+	setTimeout(() => {
+		[...document.getElementsByTagName("textarea")].map((i) => trigger(i, "input"));
+	}, 50);
+	setTimeout(SetLineNumbers, 1);
+};
+
+const SetLineNumbers = () => {
+	let count = document.body.innerHTML.match(/\r?\n\r?/g).length;
+	count += TypeTextarea.value ? TypeTextarea.value.match(/\r?\n\r?/g).length : 0;
+	count = Math.max(count, MinLineNumbers) + 1;
+	LineNumbersBox.innerHTML = "";
+	LineNumbersBox.innerText = "";
+	LineNumbersBox.append(...[...Array(count).keys()].slice(1).map((i) => <p class="Line">{i}</p>));
 };
 
 document.addEventListener("keydown", (event) => {
@@ -45,48 +112,14 @@ document.addEventListener("keydown", (event) => {
 	}
 });
 
-$(async () => {
+docReady(async () => {
 	await LoadContent();
 
 	const PageContent = (
 		<div>
-			<div id="LineNumbers">
-				{[...Array(14 + 1).keys()].slice(1).map((i) => (
-					<p class="Line">{i}</p>
-				))}
-			</div>
+			{LineNumbersBox}
 			<div id="Content">
-				<textarea
-					id="Input"
-					spellCheck={false}
-					placeholder=" "
-					rows={1}
-					onInput={(e) => {
-						e.currentTarget.style.height = "25px";
-						e.currentTarget.style.height = e.currentTarget.scrollHeight + "px";
-						e.currentTarget.removeAttribute("rows");
-					}}
-					onKeyDown={(e) => {
-						e.currentTarget.spellcheck = false;
-						if (e.key == "Tab") {
-							// get caret position/selection
-							const target = e.currentTarget as HTMLTextAreaElement;
-							const start = target.selectionStart ?? 0;
-							const end = target.selectionEnd ?? 0;
-
-							const value = "" + e.currentTarget.value;
-
-							// set textarea value to: text before caret + tab + text after caret
-							e.currentTarget.value = value.substring(0, start) + "\t" + value.substring(end);
-
-							// put caret at right position again (add one for the tab)
-							target.selectionStart = target.selectionEnd = start + 1;
-
-							// prevent the focus lose
-							e.preventDefault();
-						}
-					}}
-				></textarea>
+				{TypeTextarea}
 				<div id="MainContent"></div>
 			</div>
 			<span id="Copyright" class="comment">
@@ -97,5 +130,5 @@ $(async () => {
 
 	document.body.append(PageContent);
 	document.title = Name;
-	LoadPage(1);
+	LoadPage(0);
 });
